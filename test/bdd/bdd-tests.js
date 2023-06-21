0a4aa8c6b46dfe3c19fbb30e84ecd54f07491eb7
phenoz <phenozd@gmail.com>
2023-06-21 17:37:38 -0400

    added a few more tests

--- test/bdd/bdd-tests.js
+++ test/bdd/bdd-tests.js
@@ -6,14 +6,14 @@ import { submitBalances } from '../_helpers/network';
 import { registerNode, setNodeTrusted, nodeDeposit, nodeStakeRPL } from '../_helpers/node';
 import { getRethExchangeRate, getRethTotalSupply, mintRPL } from '../_helpers/tokens';
 import { getDepositSetting } from '../_helpers/settings';
-import { deposit } from './scenario-deposit';
+import { deposit } from '../deposit/scenario-deposit';
 import {
     RocketDAONodeTrustedSettingsMembers,
     RocketDAOProtocolSettingsDeposit, RocketDepositPool,
 } from '../_utils/artifacts';
 import { setDAOProtocolBootstrapSetting } from '../dao/scenario-dao-protocol-bootstrap';
 import { setDAONodeTrustedBootstrapSetting } from '../dao/scenario-dao-node-trusted-bootstrap'
-import { assignDepositsV2 } from './scenario-assign-deposits-v2';
+import { assignDepositsV2 } from '../deposit/scenario-assign-deposits-v2';
 import { assertBN } from '../_helpers/bn';
 
 export default function() {
@@ -41,6 +41,20 @@ export default function() {
         });
 
 
+        describe(printTitle('staker','cannot make a deposit while deposits are disabled'), function() {
+            let depositAmount = '10'.ether;
+            it('Given that a staker tries to deposit while deposits are disabled', async function() {
+                await setDAOProtocolBootstrapSetting(RocketDAOProtocolSettingsDeposit, 'deposit.enabled', false, {from: owner});
+            }),
+          
+            it('Then the transaction should revert', async function() {
+                await shouldRevert(deposit({
+                    from: staker,
+                    value: depositAmount,
+                }), 'Made a deposit while deposits are disabled');
+            })
+        });
+
         describe(printTitle('staker','cannot make a deposit below the minimum deposit amount'), function() {
             var minimumDeposit;
             var depositAmount;
@@ -57,6 +71,22 @@ export default function() {
                     from: staker,
                     value: depositAmount,
                 }), 'Made a deposit below the minimum deposit amount');
+            })
+        });
+
+        describe(printTitle('staker','cannot make a deposit which would exceed the maximum deposit pool size'), function() {
+            let depositAmount = '100'.ether;
+            let exceedAmount = '101'.ether;
+            it('Given that a staker tries to deposit more than the maximum deposit pool size', async function() {
+                    // Set max deposit pool size
+                await setDAOProtocolBootstrapSetting(RocketDAOProtocolSettingsDeposit, 'deposit.pool.maximum', depositAmount, {from: owner});
+            }),
+          
+            it('Then the transaction should revert', async function() {
+                await shouldRevert(deposit({
+                    from: staker,
+                    value: exceedAmount,
+                }), 'Made a deposit which exceeds the maximum deposit pool size');
             })
         });
 
